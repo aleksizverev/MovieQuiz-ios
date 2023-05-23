022e8cff7fcf3da456086d10f15229663ab9cd3f
aleksizverev <aleksei.zverev@icloud.com>
2023-05-23 18:10:01 +0300

    sprint_07 before review

--- MovieQuiz/Helpers/MovieQuizViewController.swift
+++ MovieQuiz/Helpers/MovieQuizViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController {
+final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol {
+    
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var yesButton: UIButton!
